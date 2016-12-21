1fb997119d8c2e65b088184fab93a6801745e846
George G <kreach3r@users.noreply.github.com>
2016-12-21 19:24:26 +0200

    Revert "hammerhead: kill PowerHAL"
    This reverts commit 9215b6c278f0898dedbc6f84f675faa65566edcd.

--- device.mk
+++ device.mk
@@ -172,6 +172,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     keystore.msm8974
 
+PRODUCT_PACKAGES += \
+    power.msm8974
+
 # GPS configuration
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/gps.conf:system/etc/gps.conf
@@ -216,6 +219,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     bdAddrLoader
+
+PRODUCT_PACKAGES += \
+    power.hammerhead
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
